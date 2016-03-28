5fe2ef7de5e63222a9582a057b6851ab05b2af1d
Wesley Vance <wesley.vance@mac.com>
2016-03-28 17:02:38 -0500

    Added math for percent played after button click: TODO- draw circle and fill in

--- app/src/main/java/fastphrase/com/views/PlayButtonView.java
+++ app/src/main/java/fastphrase/com/views/PlayButtonView.java
@@ -2,6 +2,7 @@ package fastphrase.com.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -14,6 +15,7 @@ import fastphrase.com.R;
 public class PlayButtonView extends FrameLayout{
 
     private long mPlayLengthMs;
+    private long ellapsedPlayTimeMs = 0;
     private IPlayButtonListener mListener;
 
     public PlayButtonView(Context context) {
@@ -29,7 +31,34 @@ public class PlayButtonView extends FrameLayout{
 
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = (View) inflater.inflate(R.layout.view_play_button, this, true);
+        this.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
 
+//              NEED TO GET THE BUTTON: CALCULATE AMOUNT OF TIME REMAINING AND DISPLAY PERCENTAGE OF THAT.
+//              I BELIVE I CAN USE THIS METHOD: public void addArc (RectF oval, float startAngle, float sweepAngle);
+                long timeButtonPush = System.currentTimeMillis();
+//              PLAY THE CLIP HERE
+//              ANNIMATE HERE
+                mPlayLengthMs = 5000;
+                float percentFilled = 0;
+                while(ellapsedPlayTimeMs < mPlayLengthMs){
+
+//                  DIFFERNCE BETWEEN CURRENT TIME AND WHEN THE BUTTON WAS PUSHED
+                    long currentTime = System.currentTimeMillis();
+                    ellapsedPlayTimeMs = currentTime - timeButtonPush;
+
+//                  Calculates percent the pie should be filled: Between 0 and 100
+                    percentFilled = ((ellapsedPlayTimeMs/(float)mPlayLengthMs));
+//                  ANNIMATE BUTTON BASED OFF percentFilled: CREATE CANVAS?
+//                  http://stackoverflow.com/questions/12458476/how-to-create-circular-progress-barpie-chart-like-indicator-android
+
+                    
+
+                    Log.d("Wes", "Current Ellapsed Time: " + String.valueOf(ellapsedPlayTimeMs) + " mPlayLengthMS: " + String.valueOf(mPlayLengthMs) + " Percent Filled: " + String.valueOf(percentFilled));
+                }
+                Log.d("Wes", String.valueOf(timeButtonPush));
+            }
+        });
     }
 
     /**
