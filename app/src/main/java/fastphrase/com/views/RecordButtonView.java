285fc1baf1a66e2c3eb3188fb2bf2551089f7944
Justin Tan <justin.tan93@yahoo.com>
2016-04-05 19:27:29 -0500

    Added Record button

--- app/src/main/java/fastphrase/com/views/RecordButtonView.java
+++ app/src/main/java/fastphrase/com/views/RecordButtonView.java
@@ -1,14 +1,18 @@
 package fastphrase.com.views;
-
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ViewFlipper;
 
 import fastphrase.com.R;
+import fastphrase.com.RecordingActivity;
 
 public class RecordButtonView extends FrameLayout{
+
+    private ViewFlipper mRecordIcon;
+    private IRecordButtonListener mListener;
 
     public RecordButtonView(Context context) {
         this(context, null);
@@ -22,16 +26,33 @@ public class RecordButtonView extends FrameLayout{
         super(context, attrs, defStyleAttr);
 
         LayoutInflater inflater = LayoutInflater.from(context);
-        View view = (View) inflater.inflate(R.layout.view_record_button, this, true);
+        View view = inflater.inflate(R.layout.view_record_button, this, true);
+        mRecordIcon = (ViewFlipper) view.findViewById(R.id.record_button_flipper);
+        mRecordIcon.setInAnimation(this.getContext(), R.anim.recording_button_in);
+        mRecordIcon.setOutAnimation(this.getContext(), R.anim.recording_button_out);
 
+        this.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                if (mRecordIcon.getDisplayedChild() == 0) {
+                    mRecordIcon.showNext();
+                    if (mListener != null) {
+                        mListener.onStartRecording();
+                    }
+                } else {
+                    mRecordIcon.showPrevious();
+                    if (mListener != null) {
+                        mListener.onStopRecording();
+                    }
+                }
+            }
+        });
     }
-
     /**
      * Who is listening for the button press?
      * @param listener IRecordButtonListener
      */
     public void setRecordButtonListener(IRecordButtonListener listener){
-        // todo
+        mListener = listener;
     }
 
 
