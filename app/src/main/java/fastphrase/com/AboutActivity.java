3ca50df08a87430f3fb4acbd5dd24a402f843b5c
Justin Tan <justin.tan93@yahoo.com>
2016-04-14 18:15:40 -0500

    Removed the Audio option

--- app/src/main/java/fastphrase/com/AboutActivity.java
+++ app/src/main/java/fastphrase/com/AboutActivity.java
@@ -12,7 +12,7 @@ import fastphrase.com.helpers.SettingsHelper;
 
 public class AboutActivity extends AppCompatActivity {
 
-    private CheckBox mAudio, mHaptic;
+    private CheckBox mHaptic;
 
     public static Intent newInstance(Context context){
         Intent intent = new Intent(context, AboutActivity.class);
@@ -23,18 +23,9 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        mAudio = (CheckBox)findViewById(R.id.audio_checkbox);
         mHaptic = (CheckBox)findViewById(R.id.haptic_checkbox);
 
-        mAudio.setChecked(SettingsHelper.getAudioPreference(this));
         mHaptic.setChecked(SettingsHelper.getHapticPreference(this));
-
-        mAudio.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                SettingsHelper.onClick(buttonView.getContext(), buttonView);
-                SettingsHelper.setAudioPreference(buttonView.getContext(), isChecked);
-            }
-        });
 
         mHaptic.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
