7ac14e0d3ecafe1d69de0b955fdc19ced90e9562
Bob Timm <bajabob.tx@gmail.com>
2016-04-20 12:26:30 -0500

    Updated to version ALPHA 👏🌟💎

--- app/src/main/java/fastphrase/com/AboutActivity.java
+++ app/src/main/java/fastphrase/com/AboutActivity.java
@@ -2,11 +2,14 @@ package fastphrase.com;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import fastphrase.com.helpers.SettingsHelper;
 
@@ -33,5 +36,14 @@ public class AboutActivity extends AppCompatActivity {
                 SettingsHelper.setHapticPreference(buttonView.getContext(), isChecked);
             }
         });
+
+        try {
+            TextView version = (TextView) findViewById(R.id.version);
+            PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            String versionName = pInfo.versionName;
+            version.setText(getString(R.string.version) + versionName);
+        }catch (PackageManager.NameNotFoundException e){
+
+        }
     }
 }
